aecb53a6c33ae67e56289d6056542419ccaab7e1
keitht10uk <44155922+keitht10uk@users.noreply.github.com>
2018-11-06 18:11:01 +0000

    Add files via upload

---
+++ TechTest/AnyCompany.Tests/MainTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnyCompany;
+
+namespace AnyCompany.Tests
+{
+    [TestFixture]
+    public class MainTests
+    {
+        [Test]
+        public void PlaceOrderShould()
+        {
+            var order = new Order
+            {
+                CustomorId = 1,
+                Amount = 20
+            };
+
+            var orderService = new OrderService();
+            var sucess = orderService.PlaceOrder(order);
+            Assert.IsTrue(sucess, "Order not save sucessfully");
+        }
+
+        [Test]
+        public void GetCustomerWithOrdersShould()
+        {
+            const int customerId = 1;
+
+            var dataProvider = new DataProvider();
+            var customer = dataProvider.GetCustomerWithOrder(customerId);
+            Assert.IsNotNull(customer, "Customer not found");
+            Assert.IsTrue(customer.Orders.Any(), "No order found");
+        }
+    }
+}
